11ecd687b11e32cc0e42f386676d9f851f754f81
Sanjay Sanjay <sanjay.05@icloud.com>
2026-06-13 19:18:59 +0530

    Remove debug test scripts

--- test_conn.py
+++
@@ -1,7 +0,0 @@
-import sys
-from sqlalchemy.engine.url import make_url
-
-url1 = make_url("mysql+mysqlconnector://root:admin@127.0.0.1:3306/")
-url2 = make_url("mysql+mysqlconnector://root:admin@127.0.0.1:3306")
-print(f"URL1 database: {url1.database!r}")
-print(f"URL2 database: {url2.database!r}")
